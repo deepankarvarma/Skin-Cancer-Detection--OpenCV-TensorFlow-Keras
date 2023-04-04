bd7781cf2fe6c5ebd93c8fd207e9beaca8bb3c43
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-04 10:44:41 +0530

    imporved the model

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 import os
 import numpy as np
 import cv2
-from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.layers import BatchNormalization
 
 # Set path to the data folder
-data_dir = 'data'
+data_dir = 'Skin_Data'
 
 # Set the size of the input images
 img_size = (224, 224)
@@ -20,33 +20,47 @@ train_data_gen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=
 val_data_gen = ImageDataGenerator(rescale=1./255)
 
 # Load the images and labels from the data folder
-x = []
-y = []
-for label, folder_name in enumerate(['Non_Cancer', 'Cancer']):
+x_train = []
+y_train = []
+for label, folder_name in enumerate(['Non_Cancer/Training', 'Cancer/Training']):
     folder_path = os.path.join(data_dir, folder_name)
     for img_name in os.listdir(folder_path):
         img_path = os.path.join(folder_path, img_name)
         img = cv2.imread(img_path)
         img = cv2.resize(img, img_size)
-        x.append(img)
-        y.append(label)
+        x_train.append(img)
+        y_train.append(label)
 
-# Convert the images and labels to numpy arrays
-x = np.array(x)
-y = np.array(y)
+x_train = np.array(x_train)
+y_train = np.array(y_train)
 
-# Split the data into training and validation sets
-x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, stratify=y)
+x_val = []
+y_val = []
+for label, folder_name in enumerate(['Non_Cancer/Testing', 'Cancer/Testing']):
+    folder_path = os.path.join(data_dir, folder_name)
+    for img_name in os.listdir(folder_path):
+        img_path = os.path.join(folder_path, img_name)
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, img_size)
+        x_val.append(img)
+        y_val.append(label)
+
+x_val = np.array(x_val)
+y_val = np.array(y_val)
 
 # Define the model architecture
 model = Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(img_size[0], img_size[1], 3)))
+model.add(BatchNormalization())
 model.add(MaxPooling2D((2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
+
 model.add(MaxPooling2D((2, 2)))
 model.add(Conv2D(128, (3, 3), activation='relu'))
+
 model.add(MaxPooling2D((2, 2)))
 model.add(Conv2D(128, (3, 3), activation='relu'))
+
 model.add(MaxPooling2D((2, 2)))
 model.add(Flatten())
 model.add(Dropout(0.5))
@@ -59,7 +73,7 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']
 # Train the model
 history = model.fit(train_data_gen.flow(x_train, y_train, batch_size=batch_size),
                     validation_data=val_data_gen.flow(x_val, y_val),
-                    epochs=4, verbose=1)
+                    epochs=20, verbose=1)
 
 # Save the model
 model.save('skin_cancer_detection_model.h5')
